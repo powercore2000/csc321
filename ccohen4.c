6dc4d1b3b454c9ce8217f18c011849d517ca24e9
powercore2000 <powercore2000@gmail.com>
2024-09-18 01:03:17 +0000

    Added intial c file code

---
+++ ccohen4.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+
+	int var1 = 0;
+	bool var2 = true;
+	float var3 = 0.1;
+	char var4 = 'a';
+
+	printf("Provide integer:")
+	scanf("%d",&var1);
+
+	if(x == 5){
+		printf("Your input IS equal to 5 \n");
+	}
+	else{
+		printf("Your input is NOT equal to 5 \n");
+	}
+
+	0;
+
+
+}
